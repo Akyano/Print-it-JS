034c9ecc885bc96b40d5c7561adc5fe4e7a5da9a
akyano <akyano@akyano.fr>
2024-09-02 14:07:24 +0200

    Add javascript of carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,61 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const prev = document.querySelector('#arrow_left');
+const next = document.querySelector('#arrow_right');
+const img = document.querySelector('#banner').querySelector('img');
+const text = document.querySelector('#banner').querySelector('p');
+const dots = document.querySelector('.dots');
+let currentSlide = 0;
+
+
+slides.forEach(() => {
+	dots.innerHTML += `<span class="dot" value="${currentSlide++}"></span>`
+})
+
+const dotsClick = document.querySelectorAll('.dot');
+
+currentSlide = 0;
+
+dotsClick.forEach(el => {
+	el.addEventListener('click',dot)
+})
+
+dotsClick[0].classList.add('active');
+
+function slide(){
+	if((slides.length-1) == currentSlide )
+		currentSlide = -1
+	img.setAttribute('src', `./assets/images/slideshow/${slides[++currentSlide].image}`);
+	text.innerHTML = slides[currentSlide].tagLine
+	dotsClick[currentSlide].click()
+
+	return true;
+}
+
+function dot(event){
+	dotsClick.forEach(el => {
+		el.classList.remove('active');
+	})
+	currentSlide = event.target.getAttribute('value');
+
+	img.setAttribute('src', `./assets/images/slideshow/${slides[currentSlide].image}`);
+	text.innerHTML = slides[currentSlide].tagLine
+
+	event.target.classList.toggle('active');
+	return true;
+}
+
+function slide_left(){
+	if(currentSlide == 0)
+		currentSlide = slides.length
+	img.setAttribute('src', `./assets/images/slideshow/${slides[--currentSlide].image}`);
+	text.innerHTML = slides[currentSlide].tagLine
+	dotsClick[currentSlide].click()
+
+	return true;
+}
+
+next.addEventListener('click', slide);
+prev.addEventListener('click', slide_left);
